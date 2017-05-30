d401409c998e821d65900b861dfc17fafe1badc1
Reid Mitchell <rbm02012@mymail.pomona.edu>
2017-05-30 06:38:32 -0700

    Changes before moving to nunjucks

---
+++ README.md
@@ -0,0 +1,6 @@
+# CS 47
+
+**CS 47** is a free, online, open-source computer science textbook designed to make computer science and programming accessible to *everyone*.
+
+This project adheres to the [Open Code of Conduct][code-of-conduct]. By engaging with this project (by reading the book, by contributing to it, or by any other means), you are expected to honor this code.
+[code-of-conduct]: https://github.com/reid47/cs47/blob/master/CONDUCT.md
--- build.js
+++ build.js
@@ -1,58 +1,13 @@
 const omelet = require('C:/Users/reidm/Code/omelet/omelet/lib/omelet.js')
+const nunjucks = require('nunjucks')
 const fs = require('fs')
 const path = require('path')
-var _0777 = parseInt('0777', 8);
+
+const currentDirectory = require('process').cwd() || ''
 
 const getMetadata = (lang) => {
     return require(`./book-files/${lang}/metadata.json`)
 }
-
-// 101111
-
-function mkdirpSync (p, opts, made) {
-    if (!opts || typeof opts !== 'object') {
-        opts = { mode: opts };
-    }
-
-    var mode = opts.mode;
-    var xfs = opts.fs || fs;
-
-    if (mode === undefined) {
-        mode = _0777 & (~process.umask());
-    }
-    if (!made) made = null;
-
-    p = path.resolve(p);
-
-    try {
-        xfs.mkdirSync(p, mode);
-        made = made || p;
-    }
-    catch (err0) {
-        switch (err0.code) {
-            case 'ENOENT' :
-                made = mkdirpSync(path.dirname(p), opts, made);
-                mkdirpSync(p, opts, made);
-                break;
-
-            // In the case of any other error, just see if there's a dir
-            // there already.  If so, then hooray!  If not, then something
-            // is borked.
-            default:
-                var stat;
-                try {
-                    stat = xfs.statSync(p);
-                }
-                catch (err1) {
-                    throw err0;
-                }
-                if (!stat.isDirectory()) throw err0;
-                break;
-        }
-    }
-
-    return made;
-};
 
 const omeletOptions = {}
 
@@ -63,9 +18,9 @@ const toRender = metadata.chapters.concat([metadata.tableOfContents])
 for (let item of toRender) {
     const rendered = omelet.renderFile(
         item.inFile,
-        {"metadata": metadata},
+        { "metadata": metadata, "rootPath": currentDirectory },
         omeletOptions)
-        
+
     mkdirpSync(path.dirname(item.outFile))
     fs.writeFileSync(item.outFile, rendered, 'UTF-8')
 }
