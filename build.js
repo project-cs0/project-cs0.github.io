75f80f74e102c13dd8de6f0ce869c6956f76f7e7
Reid Mitchell <rbm02012@mymail.pomona.edu>
2017-05-30 07:20:22 -0700

    First commit with nunjucks

--- build.js
+++ build.js
@@ -1,7 +1,7 @@
-const omelet = require('C:/Users/reidm/Code/omelet/omelet/lib/omelet.js')
 const nunjucks = require('nunjucks')
 const fs = require('fs')
 const path = require('path')
+const mkdirp = require('mkdirp')
 
 const currentDirectory = require('process').cwd() || ''
 
@@ -16,11 +16,11 @@ const metadata = getMetadata('en')
 const toRender = metadata.chapters.concat([metadata.tableOfContents])
 
 for (let item of toRender) {
-    const rendered = omelet.renderFile(
-        item.inFile,
-        { "metadata": metadata, "rootPath": currentDirectory },
-        omeletOptions)
+    const rendered = nunjucks.render(item.inFile, {
+        "metadata": metadata,
+        "rootPath": currentDirectory,
+    })
 
-    mkdirpSync(path.dirname(item.outFile))
+    mkdirp.sync(path.dirname(item.outFile))
     fs.writeFileSync(item.outFile, rendered, 'UTF-8')
 }
