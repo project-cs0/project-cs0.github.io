47351824324fa3c8d55982f2d53a98cc92a5a59a
Reid Mitchell <rbm02012@mymail.pomona.edu>
2017-05-31 00:55:22 -0700

    Added acknowledgements, more improvement

--- build.js
+++ build.js
@@ -2,6 +2,13 @@ const nunjucks = require('nunjucks')
 const fs = require('fs')
 const path = require('path')
 const mkdirp = require('mkdirp')
+const beautifier = require('js-beautify')
+
+const beautifyHtml = (html) => {
+    return beautifier.html(html, {
+        "preserve_newlines": false
+    })
+}
 
 const currentDirectory = require('process').cwd() || ''
 
@@ -13,14 +20,44 @@ const omeletOptions = {}
 
 const metadata = getMetadata('en')
 
-const toRender = metadata.chapters.concat([metadata.tableOfContents])
+// Pre-process chapters so that each chapter has a reference to the previous/next chapter
+for (let i = 0; i < metadata.chapters.length; i++) {
+    if (i -1 >= 0) {
+        metadata.chapters[i].previous = metadata.chapters[i - 1]
+    }
 
-for (let item of toRender) {
-    const rendered = nunjucks.render(item.inFile, {
+    if (i + 1 < metadata.chapters.length) {
+        metadata.chapters[i].next = metadata.chapters[i + 1]
+    }
+}
+
+// Render chapters
+for (let chapter of metadata.chapters) {
+    const rendered = beautifyHtml(nunjucks.render(chapter.inFile, {
         "metadata": metadata,
         "rootPath": currentDirectory,
-    })
+        "chapterMetadata": chapter
+    }))
 
-    mkdirp.sync(path.dirname(item.outFile))
-    fs.writeFileSync(item.outFile, rendered, 'UTF-8')
+    mkdirp.sync(path.dirname(chapter.outFile))
+    fs.writeFileSync(chapter.outFile, rendered, 'UTF-8')
 }
+
+// Render other pages
+for (let page of metadata.pages) {
+    const rendered = beautifyHtml(nunjucks.render(page.inFile, {
+        "metadata": metadata,
+        "rootPath": currentDirectory,
+    }))
+
+    mkdirp.sync(path.dirname(page.outFile))
+    fs.writeFileSync(page.outFile, rendered, 'UTF-8')
+}
+
+const tblOfContents = beautifyHtml(nunjucks.render(metadata.tableOfContents.inFile, {
+    "metadata": metadata,
+    "rootPath": currentDirectory,
+}))
+
+mkdirp.sync(path.dirname(metadata.tableOfContents.outFile))
+fs.writeFileSync(metadata.tableOfContents.outFile, tblOfContents, 'UTF-8')
